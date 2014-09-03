cb61a768f2e5b9cad4b5cbe5a96323b52c1bab5b
Pedro Ferreira <plferreira4@gmail.com>
2014-09-03 11:53:32 +0100

    - add a template filter to convert minutes to day/hour/minute - use the naturaltime from django's humanize in the convert_timestamp() filter

--- steam/templatetags/steam_filters.py
+++ steam/templatetags/steam_filters.py
@@ -1,4 +1,5 @@
 from django import template
+from django.contrib.humanize.templatetags import humanize
 
 from datetime import datetime
 
@@ -14,4 +15,57 @@ def convert_timestamp( timestamp ):
     except ValueError:
         return timestamp
 
-    return datetime.fromtimestamp( date ).strftime( '%d-%m-%Y %H:%M:%S' )
+    return humanize.naturaltime( datetime.fromtimestamp( date ) )
+
+
+@register.filter
+def full_time( minutes ):
+    """
+        Converts minutes to days/hours/minutes
+
+    :param minutes:
+    :return: str
+    """
+
+    try:
+        minutes = int( minutes )
+
+    except ValueError:
+        return minutes
+
+
+        # we work in minutes
+    hour = 60
+    day = 24 * hour
+
+    hourCount = 0
+    dayCount = 0
+
+        # count the days
+    while minutes >= day:
+        dayCount += 1
+        minutes -= day
+
+        # count the hours
+    while minutes >= hour:
+        hourCount += 1
+        minutes -= hour
+
+
+    def addUnit( dateStr, value, unit ):
+
+        if value != 0:
+            if value > 1:
+                unit += 's'
+
+            return '{} {} {}'.format( dateStr, value, unit )
+
+        else:
+            return dateStr
+
+
+    time = addUnit( '', dayCount, 'day' )
+    time = addUnit( time, hourCount, 'hour' )
+    time = addUnit( time, minutes, 'minute' )
+
+    return time
