f83514fba9744a5b851d67ff40cc7126f228bab9
Pedro Ferreira <plferreira4@gmail.com>
2016-05-25 22:24:21 +0100

    - update the requirements to latest version     - start using django 1.9 - don't use string views in the urls (deprecated)

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,29 +1,32 @@
 from django.conf.urls import url
+from django.contrib.auth import views as django_views
+
+import accounts.views
 
 
 urlpatterns = [
 
-    url( r'^logout$', 'django.contrib.auth.views.logout', name= 'logout' ),
-    url( r'^user$', 'accounts.views.user_page', name= 'user_page' ),
-    url( r'^user/(?P<steamId>\d+)$', 'accounts.views.user_page', name= 'user_page' ),
-    url( r'^user/(?P<steamId>\d+)/(?P<whatToShow>\w+)$', 'accounts.views.user_page', name= 'user_page_specify' ),
+    url( r'^logout$', django_views.logout, name= 'logout' ),
+    url( r'^user$', accounts.views.user_page, name= 'user_page' ),
+    url( r'^user/(?P<steamId>\d+)$', accounts.views.user_page, name= 'user_page' ),
+    url( r'^user/(?P<steamId>\d+)/(?P<whatToShow>\w+)$', accounts.views.user_page, name= 'user_page_specify' ),
 
         # Add/Remove Moderator Rights
-    url( r'^set_moderator/confirm/(?P<username>\w+)$', 'accounts.views.set_moderator_confirm', name= 'set_moderator_confirm' ),
-    url( r'^set_moderator/(?P<username>\w+)$', 'accounts.views.set_moderator', name= 'set_moderator' ),
+    url( r'^set_moderator/confirm/(?P<username>\w+)$', accounts.views.set_moderator_confirm, name= 'set_moderator_confirm' ),
+    url( r'^set_moderator/(?P<username>\w+)$', accounts.views.set_moderator, name= 'set_moderator' ),
 
         # Remove Account
-    url( r'^remove/confirm/(?P<username>\w+)$', 'accounts.views.remove_user_confirm', name= 'remove_confirm' ),
-    url( r'^remove/(?P<username>\w+)$', 'accounts.views.remove_user', name= 'remove' ),
+    url( r'^remove/confirm/(?P<username>\w+)$', accounts.views.remove_user_confirm, name= 'remove_confirm' ),
+    url( r'^remove/(?P<username>\w+)$', accounts.views.remove_user, name= 'remove' ),
 
         # Disable Account
-    url( r'^disable/confirm/(?P<username>\w+)$', 'accounts.views.disable_user_confirm', name= 'disable_confirm' ),
-    url( r'^disable/(?P<username>\w+)$', 'accounts.views.disable_user', name= 'disable' ),
+    url( r'^disable/confirm/(?P<username>\w+)$', accounts.views.disable_user_confirm, name= 'disable_confirm' ),
+    url( r'^disable/(?P<username>\w+)$', accounts.views.disable_user, name= 'disable' ),
 
        # Private Messages
-    url( r'^message/send/(?P<username>\w+)$', 'accounts.views.message_send', name= 'message_send' ),
-    url( r'^message/all/$', 'accounts.views.message_all', name='message_all' ),
-    url( r'^message/open/(?P<messageId>\w+)$', 'accounts.views.message_open', name= 'message_open' ),
-    url( r'^message/remove_confirm/(?P<messageId>\w+)$', 'accounts.views.message_remove_confirm', name= 'message_remove_confirm' ),
-    url( r'^message/remove/(?P<messageId>\w+)$', 'accounts.views.message_remove', name= 'message_remove' ),
+    url( r'^message/send/(?P<username>\w+)$', accounts.views.message_send, name= 'message_send' ),
+    url( r'^message/all/$', accounts.views.message_all, name='message_all' ),
+    url( r'^message/open/(?P<messageId>\w+)$', accounts.views.message_open, name= 'message_open' ),
+    url( r'^message/remove_confirm/(?P<messageId>\w+)$', accounts.views.message_remove_confirm, name= 'message_remove_confirm' ),
+    url( r'^message/remove/(?P<messageId>\w+)$', accounts.views.message_remove, name= 'message_remove' ),
 ]
--- steam/urls.py
+++ steam/urls.py
@@ -1,20 +1,18 @@
-"""
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.8/topics/http/urls/
-"""
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.conf import settings
 
+import steam.views
+
 
 urlpatterns = [
 
-    url( r'^$', 'steam.views.home', name= 'home' ),
-    url( r'^home/(?P<whatToShow>\w+)$', 'steam.views.home', name= 'home_specify' ),
-    url( r'^app_list$', 'steam.views.app_list', name= 'app_list' ),
-    url( r'^find_by_id$', 'steam.views.find_by_id', name= 'find_by_id' ),
-    url( r'^game/(?P<appId>\d+)$', 'steam.views.game', name= 'game' ),
-    url( r'^game/(?P<appId>\d+)/(?P<whatToShow>\w+)$', 'steam.views.game', name= 'game_specify' ),
+    url( r'^$', steam.views.home, name= 'home' ),
+    url( r'^home/(?P<whatToShow>\w+)$', steam.views.home, name= 'home_specify' ),
+    url( r'^app_list$', steam.views.app_list, name= 'app_list' ),
+    url( r'^find_by_id$', steam.views.find_by_id, name= 'find_by_id' ),
+    url( r'^game/(?P<appId>\d+)$', steam.views.game, name= 'game' ),
+    url( r'^game/(?P<appId>\d+)/(?P<whatToShow>\w+)$', steam.views.game, name= 'game_specify' ),
 
 
     url( '', include( 'social.apps.django_app.urls', namespace= 'social' ) ),
